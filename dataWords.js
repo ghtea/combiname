45db738e3a480a1489ec6096da7132a81cd49edb
PJH <eiirwp@gmail.com>
2020-02-24 07:58:09 +0900

    q

--- dataWords.js
+++ dataWords.js
@@ -1,4 +1,4 @@
-const listAnimal =
+var listAnimal =
     ['cat', 'dog', 'fish', 'owl'];
     
-const listColor = ['black', 'red', 'white', 'silver', 'gold'];
+var listColor = ['black', 'red', 'white', 'silver', 'gold'];
